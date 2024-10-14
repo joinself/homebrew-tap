84257e9a13dd8bd6fbaa7e7644c824c2f3bf63d4
Mark Newman <mark@joinself.com>
2024-10-14 11:58:52 +0100

    initial commit

---
+++ self-sdk.rb
@@ -0,0 +1,11 @@
+class SelfSDK < Formula
+  desc "Self SDK"
+  homepage "https://www.joinself.com/"
+  url "https://ftp.gnu.org/gnu/wget/wget-1.24.5.tar.gz"
+  sha256 "fa2dc35bab5184ecbc46a9ef83def2aaaa3f4c9f3c97d4bd19dcb07d4da637de"
+  license "MIT"
+
+  def install
+    system "echo", "hello"
+  end
+end

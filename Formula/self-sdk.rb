8bcfe2fb66c5543135ae975971cf34e4d26c8aff
Mark Newman <mark@joinself.com>
2024-10-14 15:10:26 +0100

    testing

--- Formula/self-sdk.rb
+++ Formula/self-sdk.rb
@@ -1,7 +1,17 @@
+class MyDownloadStrategy < SomeHomebrewDownloadStrategy
+  def fetch(timeout: nil, **options)
+    #opoo "Unhandled options in #{self.class}#fetch: #{options.keys.join(", ")}" unless options.empty?
+
+    puts "hit"
+
+    # downloads output to `temporary_path`
+  end
+end
+
 class SelfSdk < Formula
   desc "Self SDK"
   homepage "https://www.joinself.com/"
-  url "https://artifactregistry.googleapis.com/download/v1/projects/principal-oxide-204416/locations/europe/repositories/artifacts/files/self-sdk:0.28.0:self-sdk-aarch64-apple-darwin-0.28.0.tar.gz:download", using: :curl
+  url "https://artifactregistry.googleapis.com/download/v1/projects/principal-oxide-204416/locations/europe/repositories/artifacts/files/self-sdk:0.28.0:self-sdk-aarch64-apple-darwin-0.28.0.tar.gz:download", using: MyDownloadStrategy
   sha256 "d86798f4dd32ad2841fb10819f20446d43abdeb14c5fb4a99e589c1d474189e7"
   license "MIT"
 

a438d8a560e62d05442c46a82600d934263fd3da
Mark Newman <mark@joinself.com>
2024-10-14 14:16:06 +0100

    testing

--- Formula/self-sdk.rb
+++ Formula/self-sdk.rb
@@ -8,7 +8,7 @@ class SelfSdk < Formula
   def install
     system "echo", "begin"
     system "ls -lah"
-    lib.install "libself_omemo.a"
+    lib.install "self-sdk-aarch64-apple-darwin-0.28.0/libself_sdk.a"
     system "echo", "end"
   end
 end

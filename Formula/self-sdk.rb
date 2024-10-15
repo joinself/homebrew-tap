d1f7d6294e820109fc67ababbceffb07d6e7cf65
Mark Newman <mark@joinself.com>
2024-10-15 16:00:36 +0100

    testing

--- Formula/self-sdk.rb
+++ Formula/self-sdk.rb
@@ -1,54 +1,23 @@
 require 'digest'
 
+$version = "0.28.0"
+
 class SelfSdk < Formula
   desc "Self SDK"
   homepage "https://www.joinself.com/"
-  #url "https://artifactregistry.googleapis.com/download/v1/projects/principal-oxide-204416/locations/europe/repositories/artifacts/files/self-sdk:0.28.0:self-sdk-aarch64-apple-darwin-0.28.0.tar.gz:download", using: MyDownloadStrategy
-  url "https://artifactregistry.googleapis.com/download/v1/projects/principal-oxide-204416/locations/europe/repositories/artifacts/files/self-sdk:0.28.0:self-sdk-aarch64-apple-darwin-0.28.0.tar.gz:download"
+  url "https://artifactregistry.googleapis.com/download/v1/projects/principal-oxide-204416/locations/europe/repositories/artifacts/files/self-sdk:#{version}:self-sdk-aarch64-apple-darwin-0.28.0.tar.gz:download"
   sha256 "d86798f4dd32ad2841fb10819f20446d43abdeb14c5fb4a99e589c1d474189e7"
   license "MIT"
 
   def install
-    system "echo", "begin"
-
     url = "https://artifactregistry.googleapis.com/download/v1/projects/principal-oxide-204416/locations/europe/repositories/artifacts/files/self-sdk:0.28.0:self-sdk-aarch64-apple-darwin-0.28.0.tar.gz:download"
     url_sha256 = Digest::SHA256.hexdigest(url)
-    #puts url_sha256
-
-    #puts HOMEBREW_CACHE
-
-    #pkg = Pathname.glob(HOMEBREW_CACHE/"downloads/#{url_sha256}--self-sdk:0.28.0:self-sdk-aarch64-apple-darwin-0.28.0.tar.gz:download")
-    #puts "pkg: #{pkg.dirname}/#{pkg.basename}"
 
     pkg = HOMEBREW_CACHE/"downloads/#{url_sha256}--self-sdk:0.28.0:self-sdk-aarch64-apple-darwin-0.28.0.tar.gz:download"
+    mv(pkg, "self-sdk.tar.gz")
 
-    #puts "pkg2: #{pkg2}"
-
-    #File.rename(pkg, "foo.tar.gz")
-    #mkdir(buildpath/"test")
-    mv(pkg, "foo.tar.gz")
-
-    system "tar", "-zxvf", "foo.tar.gz"
-
+    system "tar", "-zxvf", "self-sdk.tar.gz"
     lib.install "self-sdk-aarch64-apple-darwin-0.28.0/libself_sdk.a"
     include.install "self-sdk-aarch64-apple-darwin-0.28.0/self-sdk.h"
-
-    #`curl -o bar.tar.gz "https://artifactregistry.googleapis.com/download/v1/projects/principal-oxide-204416/locations/europe/repositories/artifacts/files/self-sdk:0.28.0:self-sdk-aarch64-apple-darwin-0.28.0.tar.gz:download"`
-
-    #`tar -zxvf bar.tar.gz`
-
-    #lib.install "self-sdk-aarch64-apple-darwin-0.28.0/libself_sdk.a"
-    #include.install "self-sdk-aarch64-apple-darwin-0.28.0/self-sdk.h"
-
-    #`pwd`
-    #system "pwd"
-    #`ls -lah`
-
-    # puts buildpath
-    # puts Dir["#{buildpath}/"]
-    # puts Dir.pwd
-    # system "ls -lah #{buildpath}"
-    # lib.install "self-sdk-aarch64-apple-darwin-0.28.0/libself_sdk.a"
-    system "echo", "end"
   end
 end

505aa4dc64641d1a41d32e05ef1a8b82e524d032
Mark Newman <mark@joinself.com>
2024-10-15 11:08:22 +0100

    testing

--- Formula/self-sdk.rb
+++ Formula/self-sdk.rb
@@ -37,15 +37,17 @@ class SelfSdk < Formula
     system "echo", "begin"
     system "pwd"
     url = "https://artifactregistry.googleapis.com/download/v1/projects/principal-oxide-204416/locations/europe/repositories/artifacts/files/self-sdk:0.28.0:self-sdk-aarch64-apple-darwin-0.28.0.tar.gz:download"
+
     url_sha256 = Digest::SHA256.hexdigest(url)
+    puts url_sha256
 
 
     pkg = Pathname.glob(HOMEBREW_CACHE/"downloads/#{url_sha256}--self-sdk:0.28.0:self-sdk-aarch64-apple-darwin-0.28.0.tar.gz:download")
-    
+    puts "pkg: #{pkg}"
+
     File.rename(pkg, "foo.tar.gz")
     #FileUtils.cp(pkg, 'foo.tar.gz')
 
-    puts url_sha256
     puts buildpath
     puts Dir.pwd
     system "ls -lah"

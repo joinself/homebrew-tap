aa47be4106e6983d8fe1110d94e73cb712012dd6
Mark Newman <mark@joinself.com>
2024-10-15 15:30:07 +0100

    testing

--- Formula/self-sdk.rb
+++ Formula/self-sdk.rb
@@ -28,7 +28,8 @@ end
 class SelfSdk < Formula
   desc "Self SDK"
   homepage "https://www.joinself.com/"
-  url "https://artifactregistry.googleapis.com/download/v1/projects/principal-oxide-204416/locations/europe/repositories/artifacts/files/self-sdk:0.28.0:self-sdk-aarch64-apple-darwin-0.28.0.tar.gz:download", using: MyDownloadStrategy
+  #url "https://artifactregistry.googleapis.com/download/v1/projects/principal-oxide-204416/locations/europe/repositories/artifacts/files/self-sdk:0.28.0:self-sdk-aarch64-apple-darwin-0.28.0.tar.gz:download", using: MyDownloadStrategy
+  url "https://artifactregistry.googleapis.com/download/v1/projects/principal-oxide-204416/locations/europe/repositories/artifacts/files/self-sdk:0.28.0:self-sdk-aarch64-apple-darwin-0.28.0.tar.gz:download"
   sha256 "d86798f4dd32ad2841fb10819f20446d43abdeb14c5fb4a99e589c1d474189e7"
   license "MIT"
 

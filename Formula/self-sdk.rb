3aa9e90bf17197675d1a89e4860c289aa5479d99
Mark Newman <mark@joinself.com>
2024-10-15 16:35:47 +0100

    testing

--- Formula/self-sdk.rb
+++ Formula/self-sdk.rb
@@ -11,7 +11,7 @@ class SelfSdk < Formula
     url = "https://artifactregistry.googleapis.com/download/v1/projects/principal-oxide-204416/locations/europe/repositories/artifacts/files/self-sdk:0.28.0:self-sdk-aarch64-apple-darwin-0.28.0.tar.gz:download"
     url_sha256 = Digest::SHA256.hexdigest(url)
 
-    if OS.mac?
+    if OS.linux?
       puts "I am a mac"
     end
 

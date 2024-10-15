51becb72f13049dd5e90c7ebd4251cc3ef19602c
Mark Newman <mark@joinself.com>
2024-10-15 10:53:06 +0100

    testing

--- Formula/self-sdk.rb
+++ Formula/self-sdk.rb
@@ -1,4 +1,5 @@
 require 'digest'
+require 'filutils'
 
 class MyDownloadStrategy < CurlDownloadStrategy
 
@@ -37,6 +38,12 @@ class SelfSdk < Formula
     system "pwd"
     url = "https://artifactregistry.googleapis.com/download/v1/projects/principal-oxide-204416/locations/europe/repositories/artifacts/files/self-sdk:0.28.0:self-sdk-aarch64-apple-darwin-0.28.0.tar.gz:download"
     url_sha256 = Digest::SHA256.hexdigest(url)
+
+
+    cache = Pathname.glob(HOMEBREW_CACHE/"downloads/#{url_sha256}--*")
+    puts cache
+
+
     puts url_sha256
     puts buildpath
     puts Dir.pwd

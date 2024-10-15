a89ef926adc1eb24d3aea5f65cbc7b0fd180be96
Mark Newman <mark@joinself.com>
2024-10-15 15:39:45 +0100

    testing

--- Formula/self-sdk.rb
+++ Formula/self-sdk.rb
@@ -59,6 +59,7 @@ class SelfSdk < Formula
     `tar -zxvf bar.tar.gz`
 
     lib.install "self-sdk-aarch64-apple-darwin-0.28.0/libself_sdk.a"
+    include.install "self-sdk-aarch64-apple-darwin-0.28.0/self-sdk.h"
 
     `pwd`
     system "pwd"

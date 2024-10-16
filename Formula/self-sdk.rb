679eec1778738d470391f72fbf31af75a761a375
Mark Newman <mark@joinself.com>
2024-10-16 16:32:36 +0100

    testing

--- Formula/self-sdk.rb
+++ Formula/self-sdk.rb
@@ -6,32 +6,36 @@ class SelfSdk < Formula
   license "MIT"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "foo"
+    url "https://artifactregistry.googleapis.com/download/v1/projects/principal-oxide-204416/locations/europe/repositories/releases/files/self-c-sdk:0.31.0:self-c-sdk_0.31.0_darwin_amd64.tar.gz:download"
     sha256 "da4024f7d7ccc46ed17727af1626f660a64284cadb2133324cae2e51d3878484"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "foo1"
+    url "https://artifactregistry.googleapis.com/download/v1/projects/principal-oxide-204416/locations/europe/repositories/releases/files/self-c-sdk:0.31.0:self-c-sdk_0.31.0_darwin_arm64.tar.gz:download"
     sha256 "f72263338888388f06a4799c03fcdd48c1be20bdb43f152fd520aa047775c9ef"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://artifactregistry.googleapis.com/download/v1/projects/principal-oxide-204416/locations/europe/repositories/releases/files/self-c-sdk:0.31.0:self-c-sdk_0.31.0_linux_amd64.tar.gz:download"
+    url "https://download.joinself.com/self-c-sdk/self-c-sdk_0.31.0_linux_amd64.tar.gz"
     sha256 "21c02e13ce460d9d1f8b501af286de33de14477a481a450c75ea35302c0912c9"
   end
 
   if OS.linux? && Hardware::CPU.arm?
-    url "foo2"
+    url "https://artifactregistry.googleapis.com/download/v1/projects/principal-oxide-204416/locations/europe/repositories/releases/files/self-c-sdk:0.31.0:self-c-sdk_0.31.0_linux_arm64.tar.gz:download"
     sha256 "ee7345be100cc3c21f93993e91d21a1bb76284ac74bfe8e8d8909a1987ae8b32"
   end
 
   def install
     if OS.mac? && Hardware::CPU.intel?
-      arch ""
+      url = "https://artifactregistry.googleapis.com/download/v1/projects/principal-oxide-204416/locations/europe/repositories/releases/files/self-c-sdk:0.31.0:self-c-sdk_0.31.0_darwin_amd64.tar.gz:download"
+      filename = ""
+      wd = ""
     end
 
     if OS.mac? && Hardware::CPU.arm?
-      arch ""
+      url = "https://artifactregistry.googleapis.com/download/v1/projects/principal-oxide-204416/locations/europe/repositories/releases/files/self-c-sdk:0.31.0:self-c-sdk_0.31.0_darwin_arm64.tar.gz:download"
+      filename = ""
+      wd = ""
     end
 
     if OS.linux? && Hardware::CPU.intel?
@@ -41,16 +45,16 @@ class SelfSdk < Formula
     end
 
     if OS.linux? && Hardware::CPU.arm?
-      arch ""
+      url = "https://artifactregistry.googleapis.com/download/v1/projects/principal-oxide-204416/locations/europe/repositories/releases/files/self-c-sdk:0.31.0:self-c-sdk_0.31.0_linux_arm64.tar.gz:download"
+      filename = ""
+      wd = ""
     end
 
-    #puts "homepage: #{self.url}"
+    # url_sha256 = Digest::SHA256.hexdigest(url)
+    # pkg = HOMEBREW_CACHE/"downloads/#{url_sha256}--#{filename}"
+    # mv(pkg, "self-c-sdk.tar.gz")
 
-    #url = "https://artifactregistry.googleapis.com/download/v1/projects/principal-oxide-204416/locations/europe/repositories/releases/files/self-c-sdk:0.31.0:self-c-sdk_0.31.0_linux_amd64.tar.gz:download"
-    url_sha256 = Digest::SHA256.hexdigest(url)
-    pkg = HOMEBREW_CACHE/"downloads/#{url_sha256}--#{filename}"
-    mv(pkg, "self-c-sdk.tar.gz")
-
+    system "ls"
     system "tar", "-zxvf", "self-c-sdk.tar.gz"
     lib.install "#{wd}/libself_sdk.a"
     include.install "#{wd}/self-sdk.h"

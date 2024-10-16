54dc87329b5f6a70e3a211dec80e746cec8f4d88
Mark Newman <mark@joinself.com>
2024-10-16 15:22:24 +0100

    testing

--- Formula/self-sdk.rb
+++ Formula/self-sdk.rb
@@ -6,12 +6,12 @@ class SelfSdk < Formula
   license "MIT"
 
   if OS.mac? && Hardware::CPU.intel?
-    url ""
+    url "foo"
     sha256 "da4024f7d7ccc46ed17727af1626f660a64284cadb2133324cae2e51d3878484"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url ""
+    url "foo1"
     sha256 "f72263338888388f06a4799c03fcdd48c1be20bdb43f152fd520aa047775c9ef"
   end
 
@@ -21,7 +21,7 @@ class SelfSdk < Formula
   end
 
   if OS.linux? && Hardware::CPU.arm?
-    url ""
+    url "foo2"
     sha256 "ee7345be100cc3c21f93993e91d21a1bb76284ac74bfe8e8d8909a1987ae8b32"
   end
 
@@ -42,7 +42,7 @@ class SelfSdk < Formula
       arch ""
     end
 
-    puts "homepage: #{self.homepage}"
+    puts "homepage: #{self.url}"
 
     url = "https://artifactregistry.googleapis.com/download/v1/projects/principal-oxide-204416/locations/europe/repositories/releases/files/self-c-sdk:0.31.0:self-c-sdk_0.31.0_linux_amd64.tar.gz:download"
     url_sha256 = Digest::SHA256.hexdigest(url)

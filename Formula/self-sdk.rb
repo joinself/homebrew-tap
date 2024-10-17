8bc4bef72c5348e388d762f7d97fb36d4ad0acf7
Mark Newman <mark@joinself.com>
2024-10-17 11:02:50 +0100

    testing

--- Formula/self-sdk.rb
+++ Formula/self-sdk.rb
@@ -1,27 +1,27 @@
 class SelfSdk < Formula
   desc "Self SDK"
   homepage "https://www.joinself.com/"
-  version "0.31.0"
+  version "0.34.0"
   license "MIT"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://download.joinself.com/self-c-sdk/self-c-sdk_0.31.0_darwin_amd64.tar.gz"
-    sha256 "da4024f7d7ccc46ed17727af1626f660a64284cadb2133324cae2e51d3878484"
+    url "https://download.joinself.com/self-c-sdk/self-c-sdk_0.34.0_darwin_amd64.tar.gz"
+    sha256 "e10d9e6a64392b4933a9e881ed79584993ad76af0bd8da57d155bb3a2e18a779"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://download.joinself.com/self-c-sdk/self-c-sdk_0.31.0_darwin_arm64.tar.gz"
-    sha256 "f72263338888388f06a4799c03fcdd48c1be20bdb43f152fd520aa047775c9ef"
+    url "https://download.joinself.com/self-c-sdk/self-c-sdk_0.34.0_darwin_arm64.tar.gz"
+    sha256 "1bc599b8b73a304eba9ea41db0fb919a726968c9f7e614dd5be8d9b4a6d204d5"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://download.joinself.com/self-c-sdk/self-c-sdk_0.31.0_linux_amd64.tar.gz"
-    sha256 "21c02e13ce460d9d1f8b501af286de33de14477a481a450c75ea35302c0912c9"
+    url "https://download.joinself.com/self-c-sdk/self-c-sdk_0.34.0_linux_amd64.tar.gz"
+    sha256 "1e6bd0923c38be842a0cb4849d33f516f1ff988828c4e4fdc249358e69959e9e"
   end
 
   if OS.linux? && Hardware::CPU.arm?
-    url "https://download.joinself.com/self-c-sdk/self-c-sdk_0.31.0_linux_arm64.tar.gz"
-    sha256 "ee7345be100cc3c21f93993e91d21a1bb76284ac74bfe8e8d8909a1987ae8b32"
+    url "https://download.joinself.com/self-c-sdk/self-c-sdk_0.34.0_linux_arm64.tar.gz"
+    sha256 "572de082fb3a47da89ccfdaa7563556d5ebaefc62e448ab26c7cd97e23018495"
   end
 
   def install
